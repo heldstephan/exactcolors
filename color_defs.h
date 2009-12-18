9ead5f993e5caf70d0b17381382223a632f2f8f5
Stephan Held <Stephan Held@localhost>
2009-12-18 11:19:30 -0500

    Added GPL statements.

--- color_defs.h
+++ color_defs.h
@@ -1,5 +1,21 @@
-#ifndef __COLOR_BASIS_H
-#define __COLOR_BASIS_H
+#ifndef __COLOR_DEFS_H
+#define __COLOR_DEFS_H
+/**
+    This file is part of exactcolors.
+
+    exactcolors is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    exactcolors is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with exactcolors.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <stdio.h>
 #include <limits.h>
